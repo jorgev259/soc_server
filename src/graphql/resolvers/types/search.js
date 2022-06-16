986b8ce4b45760cc21a741a195af654c1f801273
jorgev259 <jorgev259@outlook.es>
2022-06-15 20:01:04 -0600

    Filter unused classes and categories

--- src/graphql/resolvers/types/search.js
+++ src/graphql/resolvers/types/search.js
@@ -1,9 +1,11 @@
+import { classes, categories } from '../../../config/info.json'
+
 const resolvers = {
   VgmResult: {
     vgmdbUrl: parent => parent.vgmdb_url,
     releaseDate: parent => parent.release_date?.replaceAll('.', '-'),
-    categories: parent => parent.category?.split(',').map(i => i.trim()),
-    classifications: parent => parent.classification?.split(',').map(i => i.trim())
+    categories: parent => parent.category?.split(',').map(i => i.trim()).filter(c => classes.includes(c)),
+    classifications: parent => parent.classification?.split(',').map(i => i.trim()).filter(c => categories.includes(c))
   }
 }
 
