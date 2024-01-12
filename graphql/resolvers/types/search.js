bd0ab39492d4aedf94223975c366a4ce56ab208f
jorgev259 <jorgev259@outlook.es>
2024-01-11 20:02:36 -0600

    Replace web scraping with vgmdb.info

--- graphql/resolvers/types/search.js
+++ graphql/resolvers/types/search.js
@@ -4,9 +4,9 @@ const { categories, classifications } = config
 
 const resolvers = {
   VgmResult: {
-    vgmdbUrl: parent => parent.vgmdb_url,
+    vgmdbUrl: parent => parent.vgmdb_link,
     releaseDate: parent => parent.release_date?.replaceAll('.', '-'),
-    categories: parent => parent.category?.split(',').map(i => i.trim()).filter(c => categories.includes(c)),
+    categories: parent => parent.categories?.filter(c => categories.includes(c)),
     classifications: parent => parent.classification?.split(',').map(i => i.trim()).filter(c => classifications.includes(c))
   }
 }
--- utils/vgmdb.js
+++ utils/vgmdb.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+
+const artistFields = ['composers', 'lyricists', 'arrangers', 'vocals', 'performers']
 
 const isValidUrl = s => {
   try {
@@ -10,80 +11,33 @@ const isValidUrl = s => {
   }
 }
 
-async function makeRequest (url) {
+export default async function getVGMDB (search) {
+  const linkId = isValidUrl(search) ? (new URL(search)).pathname.split('/').slice(-1)[0] : search
+  let data = null
+
   try {
-    const { data } = await axios.get(
-      `https://api.nemoralni.site/albums/${url}`,
-      { headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'x-api-key': 'i-m-a-pig-i-don-t-fight-for-honor-i-fight-for-a-paycheck' } })
+    const { data: apiData } = await axios.get(`https://vgmdb.info/album/${linkId}`, { headers: { 'Content-Type': 'application/json' } })
+    data = apiData
+    const { name, names = [], discs = [] } = data
+
+    data.subTitle = Object.values(names).find(n => n !== name)
+
+    const artists = new Set()
+    artistFields.forEach(field => {
+      const list = data[field] ?? []
+      list.forEach(a => artists.add(Object.values(a.names)[0]))
+    })
+
+    data.artists = Array.from(artists)
+    data.tracklist = discs.map((disc, index) => {
+      const { tracks = [] } = disc
+      const body = tracks.map(t => Object.values(t.names)[0]).join('\n')
+
+      return { number: index, body }
+    })
 
     return data
-  } catch {
-
+  } catch (err) {
+    return data
   }
-}
-
-export default async function getVGMDB (search) {
-  const url = isValidUrl(search) ? (new URL(search)).pathname.split('/').slice(-1) : search
-
-  const data = await makeRequest(url) || {}
-
-  const { vgmdb_url: vgmdbUrl } = data
-  data.tracklist = []
-  const { data: htmlBody } = await axios.get(vgmdbUrl, { headers: { 'Content-Type': 'text/html' } })
-
-  const $ = cheerio.load(htmlBody)
-
-  const discs = $('#tracklist table')
-  discs.each((i, d) => {
-    if (d.parent.attribs.style?.includes('display: none')) return true
-
-    let list = ''
-    const tbody = d.childNodes.find(n => n.type === 'tag')
-    const trows = tbody.childNodes.filter(n => n.type === 'tag' && n.name === 'tr')
-
-    trows.forEach((tRow, i2) => {
-      if (i2 > 0) list = `${list}\n`
-      const tds = tRow.childNodes.filter(n => n.type === 'tag' && n.name === 'td')
-      const td = tds[1].childNodes[0].data.trim()
-
-      list = `${list}${td}`
-    })
-    data.tracklist.push({ number: i, body: list })
-  })
-
-  data.subTitle = $('div > span.albumtitle[style="display:inline"]')[0]?.childNodes.find(n => n.type === 'text')?.data
-
-  const creditsNodes = $('#collapse_credits tr.maincred')
-  data.artists = []
-  creditsNodes.each((i, c) => {
-    const [labelNode, nameNode] = c.children.filter(n => n.type === 'tag')
-    const creditLabel = labelNode
-      ?.children.find(c => c.children)
-      ?.children.find(c => c.children)
-      ?.children.find(s => s.attribs.class === 'artistname' && s.attribs.style?.includes('display:inline'))
-      ?.children[0].data
-
-    if (!['Vocals', 'Composer', 'Arranger'].includes(creditLabel)) return true
-
-    const artistNodes = nameNode.children
-      .filter(a => a.type === 'tag' || a.data.trim() !== ',')
-      .map(findArtist)
-      .filter(a => a.length > 0)
-
-    data.artists.push(...artistNodes.filter(a => !data.artists.includes(a)))
-  })
-
-  return data
-}
-
-function findArtist (node) {
-  if (node.type === 'text') return node.data.replace(', ', '').trim()
-
-  const result = node.children.find(ch =>
-    ch.type === 'text'
-      ? ch
-      : ch.attribs.style === 'display:inline' && ch.attribs.class === 'artistname')
-
-  if (!result) return ''
-  return result.type === 'text' ? result.data : result.children[0].data
 }
